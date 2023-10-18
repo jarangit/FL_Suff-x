65cc25bf10fa4834d13991189771e43b57e3ddcc
earth <earth@suffix.works>
2023-10-18 16:33:30 +0700

    build

--- src/components/WorksHome.js
+++ src/components/WorksHome.js
@@ -19,34 +19,12 @@ function WorksHome() {
     // const [active, setActive] = useState(null)
 
 
-    const divs = [
-        {
-            id: "Home",
-            bgColor: "grey",
-        },
-        {
-            id: "Skills",
-            bgColor: "white",
-        },
-        {
-            id: "Projects",
-            bgColor: "skyblue",
-        },
-        {
-            id: "About",
-            bgColor: "lightgreen",
-        },
-        {
-            id: "Contact",
-            bgColor: "lightsalmon",
-        },
-    ];
 
     let params = useParams();
+    let { lang } = useParams();
     const { t, i18n } = useTranslation();
     const url = "https://www.suffix.works/api-v2/home/" + params.lang + "";
     const [data, setData] = useState([]);
-    // const [lang, setLang] = useState("en");
 
 
     const getWork = () => {
@@ -86,38 +64,6 @@ function WorksHome() {
 
 
     const [currentTab, setCurrentTab] = useState('1');
-    // const tabs = [
-    //     {
-    //         id: 1,
-    //         tabTitle: 'J.P. Morgan',
-    //         image: '/images/home/works/1.jpg',
-    //         title: "Create Marketing Material for J.P. Morgan",
-    //     },
-    //     {
-    //         id: 2,
-    //         tabTitle: 'Dusit Central Park',
-    //         image: '/images/home/works/1.jpg',
-    //         title: "Create Marketing Material for J.P. Morgan",
-    //     },
-    //     {
-    //         id: 3,
-    //         tabTitle: 'TIPCO Asphalt',
-    //         image: '/images/home/works/1.jpg',
-    //         title: "Create Marketing Material for J.P. Morgan",
-    //     },
-    //     {
-    //         id: 4,
-    //         tabTitle: 'CUB house',
-    //         image: '/images/home/works/1.jpg',
-    //         title: "Create Marketing Material for J.P. Morgan",
-    //     },
-    //     {
-    //         id: 5,
-    //         tabTitle: 'First Choice',
-    //         image: '/images/home/works/1.jpg',
-    //         title: "Create Marketing Material for J.P. Morgan",
-    //     }
-    // ];
 
     const handleTabClick = (e) => {
         setCurrentTab(e.target.id);
@@ -137,17 +83,17 @@ function WorksHome() {
                     <h3>Works</h3>
                 </div>
                 <div className='wrapWorksHome'>
-                    <div className='wrapTextLeftworksHome' style={{ position: headerColor }}>
+                    {/* <div className='wrapTextLeftworksHome' style={{ position: headerColor }}>
                         <Navbar observerRefs={observerRefs} />
-                </div>
+                    </div> */}
                     <Container>
                         <Row>
                             <Col lg={6}>
-                                {/* <div className='wrapTextLeftworksHome' style={{ position: headerColor }}>
+                                <div >
                                     <div className='wrapSubTitleWorksHome'>
-                                        <h2>{data.work_description}</h2>
+                                        {/* <h2>{data.work_description}</h2> */}
                                     </div>
-                                    <Navbar observerRefs={observerRefs} />
+                                    {/* <Navbar observerRefs={observerRefs} /> */}
                                     <div className='wrapCaseStudy'>
                                         <h3>Case Study</h3>
                                         <div className='wrapListCaseStudyCase'>
@@ -160,7 +106,7 @@ function WorksHome() {
                                         </div>
 
                                     </div>
-                                </div> */}
+                                </div>
                             </Col>
                             <Col lg={6}>
                                 <div className='wrapContentCaseStudy'>
@@ -191,9 +137,7 @@ function WorksHome() {
                     </Container>
                     <div className='lineWorskHome'></div>
                 </div>
-                <a>
-                    <button>View  Our Works</button>
-                </a>
+                <Link to={`/works/${lang}`}> <button>View  Our Works</button></Link>
             </section>
         </div>
     );
