e1f80910397e95d0ae6e8a6a2fef4efb45840743
earth <earth@suffix.works>
2024-01-26 21:14:03 +0700

    fix file error

--- src/PageThinkInfo.js
+++ src/PageThinkInfo.js
@@ -2,7 +2,7 @@ import './App.scss';
 import TitleThinkInfo from './components/TitleThinkInfo.js';
 import WriterThinkInfo from './components/WriterThinkInfo.js';
 import InfoThink from './components/InfoThink.js';
-import ShareThinkInfo from './components/ShareThinkInfo.js';
+import ShareThinkInfo from './components/shareThinkInfo.js';
 import AppHeader from './components/AppHeader';
 
 function PageThinkInfo() {
