eb12dc306ef336752c4276a621e38532416e3dc8
earth <earth@suffix.works>
2024-02-05 14:26:20 +0700

    wip share

--- src/components/SEO.js
+++ src/components/SEO.js
@@ -12,13 +12,17 @@ export default function SEO({ title, description, name, type,image }) {
             <meta property="og:title" content={title} />
             <meta property="og:description" content={description} />
             <meta property="og:image" content={image} />
+            
             { /* End Facebook tags */}
             { /* Twitter tags */}
             <meta name="twitter:creator" content={name} />
             <meta name="twitter:card" content={type} />
             <meta name="twitter:title" content={title} />
+            <meta name="twitter:image"  content={image} />
+
             <meta name="twitter:description" content={description} />
             { /* End Twitter tags */}
+            
         </Helmet>
     )
 }
