31778b826734562311a5dcc426d36cc570168bdc
earth <earth@suffix.works>
2024-02-05 10:57:21 +0700

    wip share

--- src/components/InfoThink.js
+++ src/components/InfoThink.js
@@ -78,24 +78,7 @@ function InfoThink() {
                 <meta name="twitter:image" content={data.image} /> */}
 
             </Helmet>
-            <Helmet>
-                { /* Standard metadata tags */}
-                <title>{title}</title>
-                <meta name='description' content={description} />
-                { /* End standard metadata tags */}
-                { /* Facebook tags */}
-                <meta property="og:type" content={type} />
-                <meta property="og:title" content={title} />
-                <meta property="og:description" content={description} />
-                <meta property="og:image" content={image} />
-                { /* End Facebook tags */}
-                { /* Twitter tags */}
-                <meta name="twitter:creator" content={name} />
-                <meta name="twitter:card" content={type} />
-                <meta name="twitter:title" content={title} />
-                <meta name="twitter:description" content={description} />
-                { /* End Twitter tags */}
-            </Helmet>
+            
             <div className='wrapPage'>
                 {
 
