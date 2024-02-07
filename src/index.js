613ac66932c2b205041f248666ee3b67d571cf8b
earth <earth@suffix.works>
2024-02-07 17:08:42 +0700

    wip share content

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { hydrate, render } from "react-dom";
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
@@ -14,16 +15,29 @@ i18next.init({
   interpolation: { escapeValue: false },  // React already does escaping
 });
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <CookiesProvider defaultSetOptions={{ path: '/' }}>
-        <App />
-      </CookiesProvider>
-    </BrowserRouter>
-  </React.StrictMode>
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+  // <React.StrictMode>
+  //   <BrowserRouter>
+  //     <CookiesProvider defaultSetOptions={{ path: '/' }}>
+  //       <App />
+  //     </CookiesProvider>
+  //   </BrowserRouter>
+  // </React.StrictMode>
+// );
+const AppWithRouter = (
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
 );
+
+const rootElement = document.getElementById("root");
+if (rootElement.hasChildNodes()) {
+  hydrate(AppWithRouter, rootElement);
+} else {
+  render(AppWithRouter, rootElement);
+}
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
