553ecb0de5c4ba321ae26b51412ca7a948bdb73a
Nattanon093 <62237402+Nattanon093@users.noreply.github.com>
2024-02-15 16:48:01 +0700

    [update] : index.html

--- src/components/MetaTagComponent.js
+++ src/components/MetaTagComponent.js
@@ -5,7 +5,6 @@ function MetaTagComponent({ title, description, url, imageUrl, imageAlt }) {
     let replaceTitle = title?.replace(/<[^>]+>/g, '');
     let replaceDescription = description?.replace(/<[^>]+>/g, '').substring(0, 160);
     let replaceImageAlt = imageAlt?.replace(/<[^>]+>/g, '');
-    let sizeImage = imageUrl?.split('/').pop().split('.')[0];
 
     return (
         <MetaTags>
@@ -13,7 +12,7 @@ function MetaTagComponent({ title, description, url, imageUrl, imageAlt }) {
             <meta property="og:title" content={replaceTitle} />
             <meta name="description" content={replaceDescription} />
             <meta property="og:description" content={replaceDescription} />
-            <meta property="og:image" content={sizeImage} />
+            <meta property="og:image" content={imageUrl} />
             <meta property="og:url" content={url} />
             <meta name="twitter:card" content="summary_large_image" />
             <meta name="twitter:image:alt" content={replaceImageAlt} />
