830eb1e74e940499ce5ba193ae463b66ad7f6d36
jarangit <jaran.dch@gmail.com>
2024-05-16 22:54:27 +0700

    f

--- src/PageThinkInfo.js
+++ src/PageThinkInfo.js
@@ -4,10 +4,12 @@ import WriterThinkInfo from './components/WriterThinkInfo.js';
 import InfoThink from './components/InfoThink.js';
 import ShareThinkInfo from './components/shareThinkInfo.js';
 import AppHeader from './components/AppHeader';
+import HelmetMetaData from './components/HelmetMetaData.js';
 
 function PageThinkInfo() {
   return (
     <div>
+      <HelmetMetaData image={'https://suffix.works/uploads/uploads/thinks/5b9049712180e3de466ea10c9ef8fef2.jpg'}/>
       <AppHeader />
       <TitleThinkInfo />
       <InfoThink />
